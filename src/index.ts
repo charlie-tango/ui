dd8db24176d1666e07d0a07a36ca9c06b62b8950
Daniel Schmidt <dsc@charlietango.dk>
2020-01-07 15:21:57 +0100

    fix: use pkgRoot for publishing

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './Box';
 export * from './Container';
 export * from './Grid';
 export * from './Text';
+export * from './theme';
 
 export { default as sx } from './sx';
-export { baseTheme } from './theme/base';
