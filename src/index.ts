eba7f45beeaf3b780bb298e45b8bc65b3e00dbd9
Daniel Schmidt <dsc@charlietango.dk>
2020-01-08 09:40:02 +0100

    fix: modify the semantic release setup

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,19 @@ export * from './Grid';
 export * from './Text';
 export * from './theme';
 
-export { default as sx } from './sx';
+/**
+ * Export the `css` prop as `sx` to avoid confusing it with the `styled-components` css prop.
+ * This also fixes the typings to use the correct sx props, enabling it to use responsive object values.
+ * It enables you to hook into the styled-system theme on any component:
+ *
+ * @example
+ * <div css={
+ *   sx({
+ *     fontSize: 20,
+ *     color: 'red',
+ *     m: [4, 6]
+ *   })}
+ * />
+ **/
+
+export { default as sx } from '@styled-system/css';
