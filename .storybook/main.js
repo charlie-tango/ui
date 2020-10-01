d86b75d5ebc1d80105b338ff206557dd28b955dc
Daniel Schmidt <dsc@charlietango.dk>
2020-10-01 10:27:50 +0200

    build: switch to Yarn 2 pnp, see if it works

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../docs/**/*.mdx', '../src/**/*.@(story|stories).@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
